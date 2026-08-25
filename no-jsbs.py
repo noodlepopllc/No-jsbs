c0a6a887aaef89b93ab037269cdbee808f2c5ae6
Todd Thomas <noodlepopllc@gmail.com>
2026-08-25 16:31:09 -0400

    Fixed css

--- no-jsbs.py
+++ no-jsbs.py
@@ -104,7 +104,7 @@ def compile_site(siteurl='yourwebsite.com'):
         
         # 1. This is your raw, beautiful HTML body (already optimized for index.php)
         #html_body_php = markdown.markdown(raw_markdown)
-        html_body_php = markdown.markdown(raw_markdown, extensions=['fenced_code'])
+        html_body_php = markdown.markdown(raw_markdown, extensions=['fenced_code', 'codehilite'])
         
         # 💡 THE REVERSE PATCH: Find src="images/..." and convert it to src="../images/..." 
         # specifically for the physical standalone pages inside the /docs folder!
